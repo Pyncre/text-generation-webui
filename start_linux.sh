058a233412026c091fa27b461b701208d2ca84ef
Pyncre <127039008+Pyncre@users.noreply.github.com>
2025-05-27 19:00:18 +0200

    Update start_linux.sh

--- start_linux.sh
+++ start_linux.sh
@@ -4,5 +4,9 @@
 PORT=${PORT:-7860}
 echo "Starting server on port: $PORT"
 
+# Active conda et l'environnement python
+source "$CONDA_ROOT_PREFIX/etc/profile.d/conda.sh"
+conda activate "$INSTALL_ENV_DIR"
+
 # Lance le serveur avec les bons arguments
 python one_click.py --listen --listen-host 0.0.0.0 --listen-port "$PORT" "$@"
